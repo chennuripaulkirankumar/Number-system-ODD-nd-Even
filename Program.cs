37a674cc85b93c81da4b9fa7f2b140445ec43d76
paulk <paulkiran.cheuuri07@gmail.com>
2024-02-16 09:33:09 +0530

    final

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConApp;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("***Even Numbers***");
+NumberSystem.EvenNum();
+Console.WriteLine("***odd Numbers***");
+NumberSystem.OddNum();
